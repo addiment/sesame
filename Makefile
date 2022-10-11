4799f9ad671153e7bdccb278269adef5727db475
addiment <63932310+addiment@users.noreply.github.com>
2022-10-11 19:49:58 -0400

    Fix ANSI escape codes and improved builds

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
+.PHONY: DEPS
+
 CC = g++
 LD := $(CC)
 
 SRCFOLDER = ./src/
 OUTFOLDER = ./out/
-
 OUT = sesame
 OBJS = $(patsubst $(SRCFOLDER)%.cpp,$(OUTFOLDER)%.o,$(wildcard $(SRCFOLDER)*.cpp))
+
+# todo: copy these from `where`
+DEPS = SDL2.dll libgcc_s_seh-1.dll libwinpthread-1.dll libstdc++-6.dll
+# also available directly from msys2
+# https://packages.msys2.org/package/mingw-w64-x86_64-SDL2
+# https://packages.msys2.org/package/mingw-w64-x86_64-gcc-libs
+# https://packages.msys2.org/package/mingw-w64-x86_64-libwinpthread
+
 
 CFLAGS = $(shell sdl2-config --cflags)
 LDFLAGS = $(shell sdl2-config --libs)
@@ -13,7 +22,12 @@ LDFLAGS = $(shell sdl2-config --libs)
 all: $(OUT)
 
 msys: OUT := $(OUT).exe
-msys: $(OUT)
+msys: CFLAGS = $(shell pkg-config SDL2 --cflags)
+msys: LDFLAGS = $(shell pkg-config SDL2 --libs) -mconsole
+msys: $(OUT) $(DEPS)
+
+$(DEPS): %.dll:
+	cp $(shell where $@) $(OUTFOLDER)$@
 
 $(OBJS): $(OUTFOLDER)%.o: $(SRCFOLDER)%.cpp
 	@printf "\e[33mCompiling \"$@\" from \"$^\"\e[0m\n"
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <random>
 #include <SDL2/SDL.h>
 
@@ -12,6 +13,14 @@ using namespace std;
 #define VERY_STRONG_PASSWD_LENGTH 16
 #define STRONG_PASSWD_LENGTH 8
 #define WEAK_PASSWD_LENGTH 4
+
+#define ANSI_ESC_RESET "\x1b[0m"
+#define ANSI_ESC_RED "\x1b[31m"
+#define ANSI_ESC_YELLOW "\x1b[33m"
+#define ANSI_ESC_GREEN "\x1b[32m"
+#define ANSI_ESC_CYAN "\x1b[36m"
+
+bool hasTerminalColor = false;
 
 /*
 
@@ -30,9 +39,14 @@ size_t charsetlen = 74;
 int passwdLength = VERY_STRONG_PASSWD_LENGTH;
 
 int main(int argc, char* argv[]) {
-    printf("sesame \x1b[36m%u\x1b[0m.\x1b[36m%u\x1b[0m.\x1b[36m%u\x1b[0m\n", SESAME_VERSION_MAJOR, SESAME_VERSION_MINOR, SESAME_VERSION_PATCH);
+    hasTerminalColor = (SDL_getenv("TERM") != NULL || SDL_getenv("COLORTERM") != NULL);
+    
+    if (hasTerminalColor) printf("sesame \x1b[36m%u\x1b[0m.\x1b[36m%u\x1b[0m.\x1b[36m%u\x1b[0m\n", SESAME_VERSION_MAJOR, SESAME_VERSION_MINOR, SESAME_VERSION_PATCH);
+    else printf("sesame %u.%u.%u\n", SESAME_VERSION_MAJOR, SESAME_VERSION_MINOR, SESAME_VERSION_PATCH);
+    
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("\x1b[31mFailed to initialize SDL2:\x1b[0m %s\n", SDL_GetError());
+        if (hasTerminalColor) printf("\x1b[31mFailed to initialize SDL2:\x1b[0m %s\n", SDL_GetError());
+        else printf("Failed to initialize SDL2: %s\n", SDL_GetError());
         return 1;
     }
 
@@ -46,11 +60,13 @@ int main(int argc, char* argv[]) {
     }
 
     if (SDL_SetClipboardText(passwd) < 0) {
-        printf("\x1b[31mFailed to add text to clipboard:\x1b[0m %s\n", SDL_GetError());
+        if (hasTerminalColor) printf("\x1b[31mFailed to add text to clipboard:\x1b[0m %s\n", SDL_GetError());
+        else printf("Failed to add text to clipboard: %s\n", SDL_GetError());
         return 1;
     }
 
-    printf("\x1b[32mPassword copied to clipboard!\x1b[0m\n");
+    if (hasTerminalColor) printf("\x1b[32mPassword copied to clipboard!\x1b[0m\n");
+    else printf("Password copied to clipboard!\n");
 
     SDL_Quit();
     return 0;
